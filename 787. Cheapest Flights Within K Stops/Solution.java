46e7c281d0a35fe58321b9f0951dd4c523b05d2b
wangliran <wangliran@yy.com>
2022-12-01 16:44:51 +0800

    解决同一个节点多路径问题

--- 787. Cheapest Flights Within K Stops/Solution.java
+++ 787. Cheapest Flights Within K Stops/Solution.java
@@ -1,37 +1,18 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
 
-    private final static Set<String> DISCOVERY_TMP = new HashSet<>(3);
-
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[][] flights = new int[24][3];
-        flights[0] = new int[]{3, 4, 4};
-        flights[1] = new int[]{2, 5, 6};
-        flights[2] = new int[]{4, 7, 10};
-        flights[3] = new int[]{9, 6, 5};
-        flights[4] = new int[]{7, 4, 4};
-        flights[5] = new int[]{6, 2, 10};
-        flights[6] = new int[]{6, 8, 6};
-        flights[7] = new int[]{7, 9, 4};
-        flights[8] = new int[]{1, 5, 4};
-        flights[9] = new int[]{1, 0, 4};
-        flights[10] = new int[]{9, 7, 3};
-        flights[11] = new int[]{7, 0, 5};
-        flights[12] = new int[]{6, 5, 8};
-        flights[13] = new int[]{1, 7, 6};
-        flights[14] = new int[]{4, 0, 9};
-        flights[15] = new int[]{5, 9, 1};
-        flights[16] = new int[]{8, 7, 3};
-        flights[17] = new int[]{1, 2, 6};
-        flights[18] = new int[]{4, 1, 5};
-        flights[19] = new int[]{5, 2, 4};
-        flights[20] = new int[]{1, 9, 1};
-        flights[21] = new int[]{7, 8, 10};
-        flights[22] = new int[]{0, 4, 2};
-        flights[23] = new int[]{7, 2, 8};
-        int totalPrice = s.findCheapestPrice(10, flights, 6, 0, 7);
+        int[][] flights = new int[6][3];
+        flights[0] = new int[]{0, 1, 5};
+        flights[1] = new int[]{1, 2, 5};
+        flights[2] = new int[]{0, 3, 2};
+        flights[3] = new int[]{3, 1, 2};
+        flights[4] = new int[]{1, 4, 1};
+        flights[5] = new int[]{4, 2, 1};
+        int totalPrice = s.findCheapestPrice(5, flights, 0, 2, 2);
         System.out.println("需要付费金额为：" + totalPrice);
     }
 
@@ -46,31 +27,36 @@ class Solution {
      * @return
      */
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        System.out.println(String.format("cities:%s,flights:%s,src:%s,dst:%s,k:%s", n, Arrays.deepToString(flights), src, dst, k));
         // 构建航班信息图
         int[][] map = new int[n][n];
         for (int[] flight : flights) {
             map[flight[0]][flight[1]] = flight[2];
         }
         // 节点发现队列
+        Set<String> discoveryRelations = new HashSet<>();
         Queue<Integer> discovery = new LinkedList<>();
         // src 到各个城市的最低票价+换乘数（由于有可能有多种方案，这里每个节点由列表呈现）
         Map<Integer, List<Triple>> minWeights = new HashMap<>(3);
-        addNextCitiesToMinWeights(minWeights, n, map, src, null, discovery);
+        addNextCitiesToMinWeights(minWeights, n, k, map, src, dst, null, discovery, discoveryRelations);
         do {
-            System.out.println(minWeights);
-            Optional<Triple> cheapest = minWeights.values().stream()
+            System.out.println(String.format("minWeights:%s,discovery:%s", minWeights, discovery));
+            List<Triple> cheapestList = minWeights.values().stream()
                     .flatMap(Collection::stream)
                     .filter(triple -> discovery.contains(triple.city))
                     .filter(triple -> triple.stops < k)
-                    .min(Comparator.comparingInt(o -> o.price));
-            if (!cheapest.isPresent()) {
+                    .collect(Collectors.toList());
+            if (cheapestList.isEmpty()) {
                 break;
             }
-            Triple triple = cheapest.get();
-            discovery.remove(triple.city);
-            addNextCitiesToMinWeights(minWeights, n, map, src, triple, discovery);
+            cheapestList.forEach(
+                    triple -> {
+                        discovery.remove(triple.city);
+                        addNextCitiesToMinWeights(minWeights, n, k, map, src, dst, triple, discovery, discoveryRelations);
+                    }
+            );
         } while (!discovery.isEmpty());
-        System.out.println(minWeights);
+        System.out.println(String.format("minWeights:%s,discovery:%s", minWeights, discovery));
         return theCheapestPrice(minWeights, dst);
     }
 
@@ -79,16 +65,18 @@ class Solution {
      *
      * @param minWeights     minWeightMap(city->price,stops)
      * @param n              n 一共n个城市
+     * @param k              k 最大换乘次数
      * @param flights        航班信息
      * @param src            起点
      * @param curr           当前节点
      */
-    private void addNextCitiesToMinWeights(Map<Integer, List<Triple>> minWeights, int n, int[][] flights, int src, Triple curr, Queue<Integer> discovery) {
+    private void addNextCitiesToMinWeights(Map<Integer, List<Triple>> minWeights, int n, int k, int[][] flights, int src, int dst, Triple curr, Queue<Integer> discovery, Set<String> discoveryRelations) {
         int currCity = Objects.isNull(curr) ? src : curr.city;
         for (int i = 0; i < n; i++) {
             if (i != currCity) {
                 if (flights[currCity][i] > 0 && i != src) {
-                    addToDiscoveryList(discovery, currCity, i);
+                    System.out.println("addNextCitiesToMinWeights:" + currCity + "->" + i);
+                    addToDiscoveryList(discovery, currCity, i, discoveryRelations);
                     if (Objects.isNull(curr)) {
                         appendMinWeight(i, Triple.of(i, flights[currCity][i], 0), minWeights);
                     } else {
@@ -127,11 +115,11 @@ class Solution {
         return -1;
     }
 
-    private void addToDiscoveryList(Queue<Integer> discovery, int from, int tar) {
-        String tmpVal = String.format("%s-%s", from, tar);
-        if (!DISCOVERY_TMP.contains(tmpVal)) {
+    private void addToDiscoveryList(Queue<Integer> discovery, int from, int tar, Set<String> discoveryRelations) {
+        String tmpVal = String.format("%s_%s", from, tar);
+        if (!discoveryRelations.contains(tmpVal)) {
             discovery.add(tar);
-            DISCOVERY_TMP.add(tmpVal);
+            discoveryRelations.add(tmpVal);
         }
     }
 
