cd83fd273f4e2579c20e879288e9dcc67e8f7f6c
wangliran <wangliran@yy.com>
2023-03-01 19:10:47 +0800

    Two Sum

--- 1. Two Sum/Solution.java
+++ 1. Two Sum/Solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            for(int k=0;k<nums.length;k++){
-                if( k == i) continue;
-                if(nums[k]+nums[i]==target){
-                    return new int[]{i,k};
+        for (int i = 0; i < nums.length; i++) {
+            for (int k = i + 1; k < nums.length; k++) {
+                if (nums[k] + nums[i] == target) {
+                    return new int[]{i, k};
                 }
             }
         }
